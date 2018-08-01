f2ceb7fd1f7d796729cec283ebdbf6ea94a04169
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-01 21:28:49 +0530

    even iterate while loop

--- cspp1_practice/m3/iterate_even.py
+++ cspp1_practice/m3/iterate_even.py
@@ -2,8 +2,6 @@
 @author : gangadharsingh
 This program is about comparing variables
 '''
-i = 1
-while i <= 5:
-    print(i*2)
-    i += 1
+for i in range(2, 11, 2):
+    print(i)
 print("Goodbye!")
