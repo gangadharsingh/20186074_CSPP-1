1098e6554b6488f6548ce3a36b7b6c88c798829e
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-04 22:11:04 +0530

    " -> 3 of 3 passed. pylint: 10.00/10 "

--- cspp1-assignments/m5/p2/square_root.py
+++ cspp1-assignments/m5/p2/square_root.py
@@ -10,28 +10,25 @@
 # output: 6.999999999999991
 '''
 def main():
-	'''
-	# epsilon and step are initialized
-	# don't change these values
-	# your code starts here
-	'''
-	s_inp = int(input())
-	step_inp = 0.1
-	epsilon = 0.01
-	gues_a = 0
-	flag = 1
-	if flag == 1:
-		while abs(gues_a**2 - s_inp) > epsilon:
-			if gues_a < s_inp:
-				gues_a += step_inp
-			else:
-				flag =0
-	if abs(gues_a**2 - s_inp) < epsilon:
-		print(str(gues_a))
-	else:
-		print("failed")
-
-	
-
-if __name__== "__main__":
-	main()
+    '''
+    # epsilon and step are initialized
+    # don't change these values
+    # your code starts here
+    '''
+    s_inp = int(input())
+    step_inp = 0.1
+    epsilon = 0.01
+    gues_a = 0
+    flag = 1
+    if flag == 1:
+        while abs(gues_a**2 - s_inp) > epsilon:
+            if gues_a < s_inp:
+                gues_a += step_inp
+            else:
+                flag = 0
+    if abs(gues_a**2 - s_inp) < epsilon:
+        print(str(gues_a))
+    else:
+        print("failed")
+if __name__ == "__main__":
+    main()
