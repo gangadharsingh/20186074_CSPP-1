6e725a6c2b3551b5b49e9896c0e0db62b5748263
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-04 22:23:26 +0530

    " -> 0 of 3 passed. pylint: 9.29/10 "

--- cspp1-assignments/m5/p3/square_root_bisection.py
+++ cspp1-assignments/m5/p3/square_root_bisection.py
@@ -10,22 +10,22 @@
 @author: gangadhar
 '''
 def main():
-	'''
-	# epsilon and step are initialized
-	# don't change these values
-	# your code starts here
-	'''
-	s_inp = input()
-	inp_m =s_inp
-	inp_l = 1
-	epsilon = 0.1
-	inp_a = (inp_l + inp_m)/2
-	while abs(inp_m**2 - s_inp) >= epsilon:
-		if inp_m**2 < s_inp:
-			inp_l = inp_a
-		else:
-			inp_m = inp_a
-		inp_m = (inp_l + inp_m)/2
-	print(inp_a)
+    '''
+    # epsilon and step are initialized
+    # don't change these values
+    # your code starts here
+    '''
+    s_inp = input()
+    inp_m =s_inp
+    inp_l = 1
+    epsilon = 0.1
+    inp_a = (inp_l + inp_m)/2.0
+    while abs(inp_m**2 - s_inp) >= epsilon:
+        if inp_m**2 < s_inp:
+            inp_l = inp_a
+        else:
+            inp_m = inp_a
+        inp_m = (inp_l + inp_m)/2
+    print(inp_a)
 if __name__ == "__main__":
-	main()
+    main()
