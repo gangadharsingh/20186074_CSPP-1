9ddc6dddd6c84a1fb0bac401cd68912dedf289ad
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-04 11:21:44 +0530

    " -> 1 of 7 passed. pylint: 5.00/10 "

--- cspp1-assignments/m6/p3/digit_product.py
+++ cspp1-assignments/m6/p3/digit_product.py
@@ -8,8 +8,13 @@ def main():
     '''
     Read any number from the input, store it in variable int_input.
     '''
-    int_input = int(input())
-
+    int_input = str(123)#int(input())
+    
+    prod_op = 1
+    
+    for i in int_input:
+    	prod_op = int(i)*prod_op
+    print(prod_op)
 
 if __name__ == "__main__":
     main()
