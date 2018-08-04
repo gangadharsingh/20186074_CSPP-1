b88e1c818e785587fec8222f8aa210fe9b464f76
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-04 11:07:39 +0530

    " -> 0 of 7 passed. pylint: 7.86/10 "

--- cspp1-assignments/m6/p2/special_char.py
+++ cspp1-assignments/m6/p2/special_char.py
@@ -10,22 +10,19 @@ def main():
     '''
     Read string from the input, store it in variable str_input.
     '''
-    str_inp = input()
+    str_inp = 'abc@#'#input()
     str_emt = ""
-    str_cmp ="!@#$%^&*"
+    str_cmp = "!@#$%^&*"
     i = len(str_inp)
     j = 0
-    m=0
-    while j < i-1:
-        while m in str_cmp: 
-            if str_inp[j] != "!":
-                str_emt += str_inp[j]
-            else:
-                str_emt += ' '
-                str_emt += str_inp[j]
-            m=m+1
-        j += 1
+    m = 0
+    for i in str_inp:
+        if i in str_cmp:
+            str_emt += " "
+        else:
+            str_emt += i
     print(str_emt)
+
 
 if __name__ == "__main__":
     main()
