8da709a3dfb908c94d54b004ccc779a045d1e1c1
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-07 19:00:59 +0530

    " -> 7 of 10 passed. pylint: -2.22/10 "

---
+++ cspp1-assignments/m8/p1/assignment1.py
@@ -0,0 +1,26 @@
+# Exercise: Assignment-1
+# Write a Python function, factorial(n), that takes in one number and returns the factorial of given number.
+
+# This function takes in one number and returns one number.
+
+
+def fact_rial(n):
+    '''
+    n is positive Integer
+
+    returns: a positive integer, the factorial of n.
+    '''
+    # Your code here
+    if n == 1:
+    	return 1
+    else:
+    	return fact_rial(n-1) * n
+    
+
+
+def main():
+    a = input()
+    print(fact_rial(int(a)))    
+
+if __name__== "__main__":
+    main()
