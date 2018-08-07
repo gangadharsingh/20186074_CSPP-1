6b994e8bca4c4fd7e6f5404d5fcdccd068ab573b
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-07 23:51:16 +0530

    New Laptop linked

--- cspp1-assignments/m7/Functions - Assignment-3/assignment3.py
+++ cspp1-assignments/m7/Functions - Assignment-3/assignment3.py
@@ -31,22 +31,35 @@
 @author: gangadharsingh
 '''
 def paying_doiy(balance, annual_ir):
-	'''
-	'''
-	mont_pl_a = balance / 12
-	month_pl = (balance * (1 + \
-		(annual_ir/12.0)) ** 12) / 12.0
-	bal_up = balance
-	epsilon 0.0001
-	ges_a = (month_pl_a + month_pl) / 2
-	while True:
-		mnt = 1
-		while mnt <= 12:
-			bal_up = bal_up - ges_a
-			bal_up = bal_up + ((annual_ir/12.0) * bal_up)
-			mnt += 1
-		if bal_up > 0 and bal_up > epsilon:
-			
+    '''
+    What is a good upper bound? Imagine that instead of paying monthly, we paid off the entire balance at the end of the year. What we
+    ultimately pay must be greater than what we would've paid in monthly installments, because the interest was compounded on the balance
+    we didn't pay off each month. So a good upper bound for the monthly payment would be one-twelfth of the balance, after having its
+    interest compounded monthly for an entire year.
+    In short:
+    Monthly interest rate = (Annual interest rate) / 12.0
+    Monthly payment lower bound = Balance / 12
+    Monthly payment upper bound = (Balance x (1 + Monthly interest rate)12) / 12.0
+    Write a program that uses these bounds and bisection search (for more info check out the Wikipedia page on bisection search) to find
+    the smallest monthly payment to the cent (no more multiples of $10) such that we can pay off the debt within a year. Try it out with
+    large inputs, and notice how fast it is (try the same large inputs in your solution to Problem 2 to compare!). Produce the same return
+    value as you did in Assignment 2.
+
+    '''
+    mont_pl_a = balance / 12
+    month_pl = (balance * (1 + \
+        (annual_ir/12.0)) ** 12) / 12.0
+    bal_up = balance
+    epsilon 0.0001
+    ges_a = (month_pl_a + month_pl) / 2
+    while True:
+        mnt = 1
+        while mnt <= 12:
+            bal_up = bal_up - ges_a
+            bal_up = bal_up + ((annual_ir/12.0) * bal_up)
+            mnt += 1
+        if bal_up > 0 and bal_up > epsilon:
+            
 def main():
     data = input()
     # data = "4773 0.2"
