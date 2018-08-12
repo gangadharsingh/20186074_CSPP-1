e30bbf107be7b52ff46b3367c0b1ee7b337ff863
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-12 15:46:50 +0530

    " -> 9 of 9 passed. pylint: 10.00/10 "

--- cspp1-assignments/m11/p1/assignment1.py
+++ cspp1-assignments/m11/p1/assignment1.py
@@ -9,7 +9,7 @@ SCRABBLE_LETTER_VALUES = {
     'j': 8, 'k': 5, 'l': 1, 'm': 3, 'n': 1, 'o': 1, 'p': 3, 'q': 10, 'r': 1,
     's': 1, 't': 1, 'u': 1, 'v': 4, 'w': 4, 'x': 8, 'y': 4, 'z': 10
 }
-def get_word_score(word, n):
+def get_word_score(word, n_inp):
     """
     Returns the score for a word. Assumes the word is a valid word.
 
@@ -28,9 +28,8 @@ def get_word_score(word, n):
     cnt = 0
     for i in word:
         cnt += SCRABBLE_LETTER_VALUES[i]
-    if len(word) == n:
+    if len(word) == n_inp:
         return cnt*len(word) + 50
-    #print(cnt)
     return cnt*len(word)
 
 
