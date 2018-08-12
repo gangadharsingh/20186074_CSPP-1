5381f234f055acb005d2a2e8af4e95b99c6a768d
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-12 16:51:14 +0530

    " -> 6 of 6 passed. pylint: 8.75/10 "

--- cspp1-assignments/m11/p2/assignment2.py
+++ cspp1-assignments/m11/p2/assignment2.py
@@ -1,13 +1,16 @@
-#Exercise: Assignment-2
-#Implement the updateHand function. Make sure this function has no side effects: i.e., it must not mutate the hand passed in. Before pasting your function definition here, be sure you've passed the appropriate tests in test_ps4a.py.
+'''#Exercise: Assignment-2
+#Implement the updateHand function. Make sure this function has no side
+effects: i.e., it must not mutate the hand passed in. Before pasting your function
+definition here, be sure you've passed the appropriate tests in test_ps4a.py.
+@author: gangadharsingh
+'''
 
-
-def updateHand(hand, word):
+def update_Hand(hand, word):
     """
     Assumes that 'hand' has all the letters in word.
     In other words, this assumes that however many times
     a letter appears in 'word', 'hand' has at least as
-    many of that letter in it. 
+    many of that letter in it.
 
     Updates the hand: uses up the letters in the given word
     and returns the new hand, without those letters in it.
@@ -15,7 +18,7 @@ def updateHand(hand, word):
     Has no side effects: does not modify hand.
 
     word: string
-    hand: dictionary (string -> int)    
+    hand: dictionary (string -> int)
     returns: dictionary (string -> int)
     """
     # TO DO ... <-- Remove this comment when you code this function
@@ -25,16 +28,16 @@ def updateHand(hand, word):
     return hand
 
 def main():
-	n = input()
-	adict = {}
-	for i in range(int(n)):
-		data = input()
-		l = data.split()
-		adict[l[0]] = int(l[1])
-	data1 = input()
-	print(updateHand(adict, data1))
-		
-
+    '''Main function
+    '''
+    n_inp = input()
+    adict = {}
+    for data in range(int(n_inp)):
+        data = input()
+        l_inp = data.split()
+        adict[l_inp[0]] = int(l_inp[1])
+    data1 = input()
+    print(update_Hand(adict, data1))
 
 if __name__== "__main__":
-	main()
+    main()
