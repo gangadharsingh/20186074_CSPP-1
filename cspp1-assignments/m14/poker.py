f43dbb8e64128165bded5c1ea0f8a53b87a9d775
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-15 15:15:07 +0530

    " -> 0 of 8 passed. pylint: 0/0 "

--- cspp1-assignments/m14/poker.py
+++ cspp1-assignments/m14/poker.py
@@ -3,7 +3,7 @@
     Read about poker hands here.
     https://en.wikipedia.org/wiki/List_of_poker_hands
 '''
-
+dict_inp = {'A':14, 'K':13, 'Q':12, 'J':11, 'T':10, '9':9, '8':8, '7':7, '6':6, '5':5, '4':4, '3':3, '2':2}
 def is_straight(hand):
     '''
         How do we find out if the given hand is a straight?
@@ -14,6 +14,7 @@ def is_straight(hand):
         Think of an algorithm: given the card face value how to check if it a straight
         Write the code for it and return True if it is a straight else return False
     '''
+    print("is_straight")
     if all(True if c in "2345A" else False for c, s in hand):
         return True
     card_values = set(['--23456789TJQKA'.index(c) for c, s in hand])
@@ -29,12 +30,43 @@ def is_flush(hand):
         Think of an algorithm: given the card suite how to check if it is a flush
         Write the code for it and return True if it is a flush else return False
     '''
+    print("is_flush")
     suit = hand[0]
     for i in hand:
         if suit[1] != i[1]:
             return False
     return True
 
+def four_ofakind(hand):
+    face_values1 = []
+    for i in hand:
+        face_values1.append(dict_inp[i[0]])
+    face_values1.sort()
+    for i in range(len(face_values1)):
+        if face_values1[i+1]-face_values1[i] == 0:
+            count +=1
+    return count == 3
+    
+
+def three_ofakind(hand):
+    face_values1 = []
+    for i in hand:
+        face_values1.append(dict_inp[i[0]])
+    face_values1.sort()
+    for i in range(len(face_values1)):
+        if face_values1[i+1]-face_values1[i] == 0:
+            count +=1
+    return count == 2
+
+def pair_ofakind(hand):
+    face_values1 = []
+    for i in hand:
+        face_values1.append(dict_inp[i[0]])
+    face_values1.sort()
+    for i in range(len(face_values1)):
+        if face_values1[i+1]-face_values1[i] == 0:
+            count +=1
+    return count == 1
 
 def hand_rank(hand):
     '''
@@ -62,10 +94,16 @@ def hand_rank(hand):
     # any other hand would be the fourth best with the return value 0
     # max in poker function uses these return values to select the best hand
     if is_straight(hand) and is_flush(hand):
-        return 3
+        return 6
+    elif four_ofakind(hand):
+        return 5
     elif is_flush(hand):
-        return 2
+        return 4
     elif is_straight(hand):
+        return 3
+    elif three_ofakind(hand):
+        return 2
+    elif pair_ofakind(hand):
         return 1
     else:
         return 0
@@ -102,4 +140,8 @@ if __name__ == "__main__":
         ha = line.split(" ")
         HANDS.append(ha)
     # test the poker function to see how it works
+
+    for i in range(0,len(face_values)-1):
+        if face_values[i+1] == face_values[i]:
+
     print(' '.join(poker(HANDS)))
