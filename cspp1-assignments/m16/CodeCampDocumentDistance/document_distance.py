6cfcb1222e906f02dafef4368ceb19f82179ef66
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-17 16:15:11 +0530

    " -> 0 of 3 passed. pylint: -0.85/10 "

---
+++ cspp1-assignments/m16/CodeCampDocumentDistance/document_distance.py
@@ -0,0 +1,76 @@
+'''
+    Document Distance - A detailed description is given in the PDF
+'''
+import re
+import math
+def combine_dictionary(dict_one, dict_two):
+	dict_combine = {}
+	for word in dict_one and dict_two:
+		dict_combine[word] = [dict_one[word], dict_two[word]]
+
+	for word in dict_one:
+		if word not in dict_combine:
+			dict_combine[word] = [dict_one[word], 0]
+	for word in dict_two:
+		if word not in dict_combine:
+			dict_combine[word] = [0, dict_two[word]]
+	return dict_combine
+
+def calculate_similarity(dict_cal):
+	numerator = sum(key[0] * key[1] for key in dict_cal)
+	denominator_one = math.sqrt(sum(key[0]**2 for key in dict_cal))
+	denominator_one = math.sqrt(sum(key[0]**2 for key in dict_cal))
+
+def create_dic(words_l):
+	'''return dictionary and input as wordlist
+	'''
+	dict_n = {}
+	stopwords = load_stopwords("stopwords.txt")
+	for word in words_l:
+		word = word.strip()
+		if word not in stopwords and len(word) > 0:
+			if word not in dict_n:
+				dict_n[word] = 1
+			else:
+				dict_n[word] += 1
+	return dict_n 
+
+
+def clean_string(inp_1):
+	'''take string and return list
+	'''
+	words = inp_1.lower().strip()
+	regex = re.compile('[â-z]')
+	words = regex.sub(" ", words).split(" ")
+	return words
+
+def similarity(string1, string2):
+    '''
+        Compute the document distance as given in the PDF
+    '''
+    words_list1 = create_dic(clean_string(dict1))
+    words_list2 = create_dic(clean_string(dict2))
+
+    
+
+def load_stopwords(filename):
+    '''
+        loads stop words from a file and returns a dictionary
+    '''
+    stopwords = {}
+    with open(filename, 'r') as filename:
+        for line in filename:
+            stopwords[line.strip()] = 0
+    return stopwords
+
+def main():
+    '''
+        take two inputs and call the similarity function
+    '''
+    input1 = input()
+    input2 = input()
+
+    print(similarity(input1, input2))
+
+if __name__ == '__main__':
+    main()
