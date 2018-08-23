5a42f3e2c5b0faafc36613286e62e1b7fd95dac7
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-23 16:36:27 +0530

    " -> 0 of 8 passed. pylint: -2.37/10 "

--- cspp1-assignments/m20/matrix_operations.py
+++ cspp1-assignments/m20/matrix_operations.py
@@ -25,16 +25,17 @@ def add_matrix(m1, m2):
         error message should be "Error: Matrix shapes invalid for addition"
     '''
     sum_m = []
-    if len(m1) == len(m2) and len(m1[0]) == len(m2[0]):
-        for i in range(len(m1)):
-            for j in range(len(m1[i])):
-                sum_m.append(m1[i][j] + m2[i][j])
-        return [sum_m[cnt:cnt+len(m1[0])] for cnt in range(0, len(sum_m), len(m1[0]))]
-    else:
+    try:    
+        if len(m1) == len(m2) and len(m1[0]) == len(m2[0]):
+            for i in range(len(m1)):
+                for j in range(len(m1[i])):
+                    sum_m.append(m1[i][j] + m2[i][j])
+            return [sum_m[cnt:cnt+len(m1[0])] for cnt in range(0, len(sum_m), len(m1[0]))]
+    except :
         print("Error: Matrix shapes invalid for addition")
         return None
 
-def read_matrix(num):
+def read_matrix():
     '''
         read the matrix dimensions from input
         create a list of lists and read the numbers into it
@@ -42,21 +43,34 @@ def read_matrix(num):
         print an error message and return None
         error message should be "Error: Invalid input for the matrix"
     '''
-    mat = []
-    for i in range(num):
-        mat.append(input().split(' '))
-    mat_n = []
-    for j in range(len(mat)):
-        mat_n.append([int(i) for i in mat[j]])
-    return mat_n
+    m = []
+    list_inp= input().split(',')
+    r, c = int(list_inp[0]), int(list_inp[1])
+    for _ in range(r):
+        l_mat_r = input().split()
+        if c == len(l_mat_r):
+            m.append(int(i) for i in l_mat_r)
+        else:
+            print("Error: Invalid input for the matrix")
+    return m
+    # mat = []
+    # for i in range(num):
+    #     mat.append(input().split(' '))
+    # mat_n = []
+    # for j in range(len(mat)):
+    #     mat_n.append([int(i) for i in mat[j]])
+    # return mat_n
 
 def main():
     # read matrix 1
-    size = input().split(',')
-    mat_1 = read_matrix(int(size[0]))
+    mat_one = read_matrix()
+    # size = input().split(',')
+    # mat_1 = read_matrix(int(size[0]))
     # read matrix 2
-    size1 = input().split(',')
-    mat_2 = read_matrix(int(size1[0]))
+    mat_two = read_matrix()
+    print(mat_one,mat_two)
+    # size1 = input().split(',')
+    # mat_2 = read_matrix(int(size1[0]))
     # add matrix 1 and matrix 2
     if size == size1:
         print(add_matrix(mat_1, mat_2))
