b2aee05a5bd282bf9f2430e370288cd97e24984c
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-24 17:08:54 +0530

    " -> 14 of 14 passed. pylint: 9.43/10 "

--- cspp1-assignments/m21/tic_tac_toe.py
+++ cspp1-assignments/m21/tic_tac_toe.py
@@ -47,6 +47,6 @@ def win_tictactoe(tic_tactoe):
         return 'o'
     elif tic_tactoe[0][0] == tic_tactoe[1][1] == tic_tactoe[2][2]:
         return 'tic_tactoe[0][0]'
-    elif tic_tactoe[0][2] == tic_tactoe[1][1] == tic_tactoe[2][0]:
+    if tic_tactoe[0][2] == tic_tactoe[1][1] == tic_tactoe[2][0]:
         return 'tic_tactoe[0][2]'
 main()
