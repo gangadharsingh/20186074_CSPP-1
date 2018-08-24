d9137b8638a5f22ed33d1e163171837f6c63bc44
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-24 14:36:28 +0530

    " -> 14 of 14 passed. pylint: 8.29/10 "

--- cspp1-assignments/m21/tic_tac_toe.py
+++ cspp1-assignments/m21/tic_tac_toe.py
@@ -1,7 +1,8 @@
 def main():
+    '''tic tac toe game
+    '''
     #making tictactoe
     empt_tic = empt_tictactoe()
-    # print(empt_tic)
     #checking input is valid or not
     if inp_validation(empt_tic) == True:
         print(win_tictactoe(empt_tic))
@@ -9,16 +10,18 @@ def main():
         print(inp_validation(empt_tic))
 
 def empt_tictactoe():
-    '''
+    '''creating empty tic tac toe
     '''
     row = []
-    for i in range(3):
+    for _ in range(3):
         list_temp = input().split()
         row.append(list_temp)
     return row
     #either x or y count should not be greater than 5
 
 def inp_validation(tic_tactoe):
+    '''checking whether tic tac toe is valid or not
+    '''
     if sum(i.count('x') for i in tic_tactoe) > 5 or sum(i.count('o') for i in tic_tactoe) > 5:
         return "invalid game"
     for i in range(len(tic_tactoe)):
@@ -28,14 +31,12 @@ def inp_validation(tic_tactoe):
     return True
 
 def win_tictactoe(tic_tactoe):
-    # for i in range(len(tic_tactoe)):
-    #     for j in range(len(tic_tactoe)):
-    #         if sum(cnt.count(tic_tactoe[i][j]) for cnt in tic_tactoe[i]) == 3:
-    #             return tic_tactoe[i][j]
+    '''checking wining team
+    '''
     c_x = sum([tic_tactoe[i].count('x') for i in range(len(tic_tactoe))])
     c_o = sum([tic_tactoe[i].count('o') for i in range(len(tic_tactoe))])
     if c_x == 3 and c_o == 3:
-        return 'invalid game'    
+        return 'invalid game'
     if c_x == 3:
         return 'x'
     elif c_o == 3:
