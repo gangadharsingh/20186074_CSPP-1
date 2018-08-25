5f9325d40e01803da4b7fd10862bfc07f2c0e7a2
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-25 12:46:28 +0530

    " -> 0 of 26 passed. pylint: 0/0 "

---
+++ cspp1-assignments/m22/Check Sudoku/check_sudoku.py
@@ -0,0 +1,49 @@
+'''
+    Sudoku is a logic-based, combinatorial number-placement puzzle.
+    The objective is to fill a 9×9 grid with digits so that
+    each column, each row, and each of the nine 3×3 subgrids that compose the grid
+    contains all of the digits from 1 to 9.
+
+    Complete the check_sudoku function to check if the given grid
+    satisfies all the sudoku rules given in the statement above.
+'''
+
+def check_sudoku(sudoku):
+    '''
+        Your solution goes here. You may add other helper functions as needed.
+        The function has to return True for a valid sudoku grid and false otherwise
+    '''
+    for j in range(len(sudoku)):
+        if sum([int(sudoku[j][i]) for i in range(0, len(sudoku))]) != 45:
+            return 'False'
+    sudoku_transpose = transpose_matrix(sudoku)
+    for j in range(len(sudoku_transpose)):
+        if '\n',sum([int(sudoku_transpose[j][i]) for i in range(0, len(sudoku_transpose))]) != 45:
+            return 'False'
+    return 'True'
+def transpose_matrix(sudoku):
+    tran_mat = [[sudoku[j][i] for j in range(len(sudoku))] for i in range(len(sudoku[0]))]
+    trans_new = []
+    for trans in tran_mat:
+        trans_new.append(trans)
+    return trans_new
+def main():
+    '''
+        main function to read input sudoku from console
+        call check_sudoku function and print the result to console
+    '''
+    
+    # initialize empty list
+    sudoku = []
+
+    # loop to read 9 lines of input from console
+    for i in range(9):
+        # read a line, split it on SPACE and append row to list
+        row = input().split(' ')
+        sudoku.append(row)
+    print(sudoku)
+    # call solution function and print result to console
+    print(check_sudoku(sudoku))
+
+if __name__ == '__main__':
+    main()
