c074b4167eb7f3b88cb37af693d7da8090c73bf5
gangadharsingh <gangadharsingh056@msitprogram.net>
2018-08-26 03:14:31 +0530

    " -> 22 of 26 passed. pylint: 9.23/10 "

--- cspp1-assignments/m22/Check Sudoku/check_sudoku.py
+++ cspp1-assignments/m22/Check Sudoku/check_sudoku.py
@@ -17,14 +17,12 @@ def check_sudoku(sudoku):
         for i in range(len(sudoku)):
             if not sudoku[j][i] not in '123456789' or len(set(sudoku[j])) != 9:
                 return 'False'
-            # else:
-            #     return 'True'
-    #     elif sum([int(sudoku[j][i]) for i in range(len(sudoku))]) != 45:
-    #         return 'False'
-    # sudoku_transpose = transpose_matrix(sudoku)
-    # for j in range(len(sudoku_transpose)):
-    #     if sum([int(sudoku_transpose[j][i]) for i in range(len(sudoku_transpose))]) != 45:
-    #         return 'False'
+            elif sum([int(sudoku[j][i]) for i in range(len(sudoku))]) != 45:
+                return 'False'
+    sudoku_transpose = transpose_matrix(sudoku)
+    for j in range(len(sudoku_transpose)):
+        if sum([int(sudoku_transpose[j][i]) for i in range(len(sudoku_transpose))]) != 45:
+            return 'False'
     return 'True'
 def transpose_matrix(sudoku):
     '''transposing the matrix
